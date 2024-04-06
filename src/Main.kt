baafcee7777492a875dffe1afb377fb93d7a0277
Faith-Ndanu <faithndanumunyao@gmail.com>
2024-04-06 21:39:18 +0300

    added content

--- src/Main.kt
+++ src/Main.kt
@@ -1,31 +1,52 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-
-    noteApp("text")
-    noteApp("words")
-    noteApp("images")
-    noteApp("texts")
-
-    noteApp("deleteContent")
+    val notes = NoteApp("writeText","createWords","editText","blue","newText","addImages")
+    notes.images()
+    notes.text()
+    notes.color()
+    notes.words()
+    notes.edit()
 }
-fun noteApp(notes:String,){
-    if(notes=="text"){
-        println("newText")
-    }
-    else if(notes=="words"){
-        println("addText")
-    }
-    else if(notes=="texts"){
-        println("editText")
-    }
-    else if(notes=="images") {
-        println("addImages")
-    }
-    else{
-        println("deleteContent")
-    }
 
+class NoteApp(var text: String, var words: String, var edit: String, var color: String, var words2:String,var images:String) {
+    fun text() {
+        if (text == "writeText") {
+            println("saveText")
+        } else {
+            println("unSaved")
+        }
+    }
+    fun words() {
+        if (words == "createWords") {
+            println("addedWords")
+        } else {
+            println("NoNotes")
+        }
+    }
+    fun images() {
+        if (images == "image") {
+            println("imageAdded")
+        } else {
+            println("ImageNotFound")
+        }
+    }
+    fun edit() {
+        if (edit == "editText") {
+            println("edit and add content")
+        } else {
+            println("viewContent")
+        }
+    }
+    fun color() {
+        if (words2 == "new") {
+            println("changeColor")
+        } else {
+            println("normalColor")
+        }
+
+
+    }
 }
 
 
